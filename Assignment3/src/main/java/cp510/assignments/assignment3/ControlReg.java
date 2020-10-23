b0b2265b189d8ccd5e09eab155bd4a5155fd5d74
TOBY PETERSON <tspeterson206@gmail.com>
2020-10-22 22:21:45 -0700

    assignment3

---
+++ Assignment3/src/main/java/cp510/assignments/assignment3/ControlReg.java
@@ -0,0 +1,227 @@
+package cp510.assignments.assignment3;
+
+public class ControlReg {
+
+    /**
+     * The ControlReg class for UW java 510.
+     * 
+     * The ControlReg class has methods to both encode and decode a 16-bit
+     * register.
+     * 
+     * @author Toby Peterson.
+     */
+
+    private int origin;
+    private int security;
+    private int data;
+    private int count;
+    private int start;
+    private int control;
+
+    private int originMask = 0b000000000000111;
+    private int securityMask = 0b000000000011000;
+    private int dataMask = 0b0000000111100000;
+    private int countMask = 0b000011000000000;
+    private int startMask = 0b0000100000000000;
+    private int controlMask = 0b1111000000000000;
+
+    /**
+     * Main application entry point.
+     *
+     * @param args command line arguments; not used
+     */
+
+//    public static void main(String[] args) {
+//
+////        ControlReg reg = new ControlReg(0xA565);
+//    }
+
+    /**
+     * ControlReg constructor.
+     * 
+     * @param reg A binary 16-bit register.
+     */
+
+    public ControlReg(int reg) {
+//        System.out.println("reg" + Integer.toBinaryString(reg));
+        decodeReg(reg);
+        encodeReg();
+//        System.out.println("encoded " + encodeReg());
+    }
+
+    /**
+     * The getOrigin getter.
+     * 
+     * @return An integer representing the origin value of the inputed register
+     *         (in decimal).
+     */
+    public int getOrigin() {
+        return this.origin;
+    };
+
+    /**
+     * The setOrigin setter.
+     * 
+     * @param origin The decimal value of the origin portion of the register to
+     *               be set.
+     */
+
+    public void setOrigin(int origin) {
+        this.origin = origin;
+    };
+
+    /**
+     * The getSecurity getter.
+     * 
+     * @return An integer representing the security value of the inputed
+     *         register (in decimal).
+     */
+
+    public int getSecurity() {
+        return this.security;
+    };
+
+    /**
+     * The setSecurity setter.
+     * 
+     * @param security The decimal value of the security to be set.
+     */
+    public void setSecurity(int security) {
+        this.security = security;
+    };
+
+    /**
+     * The getData getter.
+     * 
+     * @return An integer representing the data value of the inputed register
+     *         (in decimal).
+     */
+    public int getData() {
+        return this.data;
+    };
+
+    /**
+     * The setData setter.
+     * 
+     * @param data The decimal value of the data portion of the register to be
+     *             set.
+     */
+    public void setData(int data) {
+        this.data = data;
+    };
+
+    /**
+     * The getCount getter.
+     * 
+     * @return An integer representing the count value of the inputed register
+     *         (in decimal).
+     */
+
+    public int getCount() {
+        return this.count;
+    };
+
+    /**
+     * The setCount setter.
+     * 
+     * @param count The decimal value of the count portion of the register to be
+     *              set.
+     */
+
+    public void setCount(int count) {
+        this.count = count;
+    };
+
+    /**
+     * The getStart getter.
+     * 
+     * @return An integer representing the start value of the inputed register
+     *         (in decimal).
+     */
+
+    public int getStart() {
+        return this.start;
+    };
+
+    /**
+     * The setStart setter.
+     * 
+     * @param start The decimal value of the start portion of the register to be
+     *              set.
+     */
+    public void setStart(int start) {
+        this.start = start;
+    };
+
+    /**
+     * The getControl getter.
+     * 
+     * @return An integer representing the control value of the inputed register
+     *         (in decimal).
+     */
+
+    public int getControl() {
+        return this.control;
+    };
+
+    /**
+     * The setControl setter.
+     * 
+     * @param control The decimal value of the control portion of the register
+     *                to be set.
+     */
+
+    public void setControl(int control) {
+        this.control = control;
+    };
+
+    /**
+     * The decodeReg function that decodes a given register and uses the decoded
+     * values to set this object's properties.
+     * 
+     * @param reg An encoded 16-bit register.
+     */
+    public void decodeReg(int reg) {
+        origin = (reg & originMask) >> 0;
+        security = (reg & securityMask) >> 3;
+        data = (reg & dataMask) >> 5;
+        count = (reg & countMask) >> 9;
+        start = (reg & startMask) >> 11;
+        control = (reg & controlMask) >> 12;
+    };
+
+    /**
+     * The encodeReg method that encodes the encapsulated properties into an
+     * integer value.
+     * 
+     * @return An integer that is the encoded value of the inputed register.
+     */
+
+    public int encodeReg() {
+
+        int reg = 0;
+        reg = reg | control << 12;
+        reg = reg | start << 11;
+        reg = reg | count << 9;
+        reg = reg | data << 5;
+        reg = reg | security << 3;
+        reg = reg | origin << 0;
+//        System.out.println("encodedreg: " + Integer.toBinaryString(reg));
+//        System.out.println("encodedregint: " + reg);
+
+        return reg;
+    };
+
+    /**
+     * The toString method that outputs a string representing the individual
+     * properties that comprise the register.
+     * 
+     * @return A human readable string displaying the 6 fields of the inputed
+     *         register.
+     */
+    public String toString() {
+        return "origin=" + origin + ",security=" + security + ",data=" + data
+        + ",count=" + count + ",start=" + start + ",control=" + control;
+    }
+
+}
--- cp510/src/main/java/cp510/assignments/assignment2/Numbers.java
+++ cp510/src/main/java/cp510/assignments/assignment2/Numbers.java
@@ -1,38 +1,25 @@
 package cp510.assignments.assignment2;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+
+/**
+ * The Numbers class for UW Java 510
+ * 
+ * @author Toby Peterson
+ *
+ */
 
 public class Numbers {
 
     public static void main(String[] args) {
-
-        boolean result = isPrime(5);
-        System.out.println("result of isPrime(5) " + result);
-
-        int result2 = lcm(210, 16);
-        System.out.println("result of lcm(210,16) " + result2);
-
-        int result3 = gcf(125, 55);
-        System.out.println("result of  gcf(125,55)" + result3);
-
-        double[] array1 = { 1.0, 1.0, 0.5, -2.2, -0.3, 0.5, -0.4, 3.2 };
-        double result4 = mean(array1);
-        System.out.println("result of mean(1.0, 1.0, 0.5, -2.2, -0.3, 0.5, -0.4, 3.2) " + result4);
-
-        double[] array2 = { -0.8, -0.5, -0.12, 0.3, 0.6, 22.0 };
-        double result5 = median(array2);
-        System.out.println("result of median(-0.8, -0.5, -0.12, 0.3, 0.6, 22.0)" + result5);
-
-        long result6 = factorial(3);
-        System.out.println("result of factorial(3)" + result6);
-
-        BigInteger result7 = factorial(BigInteger.valueOf(45));
-        System.out.println("factorial of 45 : " + result7);
     };
 
+    /**
+     * Method for determing whether a given number is prime or not
+     * 
+     * @param num the given integer
+     * @return a boolean indicating whether number is prime or not
+     */
     public static boolean isPrime(int num) {
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
@@ -42,7 +29,19 @@ public class Numbers {
         return num > 1;
     };
 
+    /**
+     * Method for determining the least common multiple of two integers
+     * 
+     * @param param1 an integer
+     * @param param2 integer
+     * @return an integer that is the least common multiple
+     */
     public static int lcm(int param1, int param2) {
+        if (param1 == 0 || param2 == 0) {
+            return 0;
+        }
+        ;
+
         int larger = Math.max(param1, param2);
         int smaller = Math.min(param1, param2);
 
@@ -53,7 +52,18 @@ public class Numbers {
         return current;
     };
 
+    /**
+     * A method that determines the great common factor that two given integers have
+     * in common
+     * 
+     * @param param1 an integer
+     * @param param2 an integer
+     * @return an integer that is the greatest common factor
+     */
     public static int gcf(int param1, int param2) {
+        if (param1 == 0 || param2 == 0) {
+            return 0;
+        }
         int greatestCommon = 1;
         for (int i = 1; i <= param1 && i <= param2; i++) {
             if (param1 % i == 0 && param2 % i == 0) {
@@ -63,43 +73,61 @@ public class Numbers {
         return greatestCommon;
     };
 
+    /**
+     * A method to determine the mean of an array of numbers
+     * 
+     * @param arr an array of numbers(doubles)
+     * @return the mean of the given numbers in the supplied array
+     */
     public static double mean(double[] arr) {
-
-        Set<Double> linkedHashSet = new LinkedHashSet<Double>();
-
         double sum = 0;
-        for (double i : arr) {
-            System.out.println(i);
-            linkedHashSet.add(i);
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
-        System.out.println(linkedHashSet);
-        for (double j : linkedHashSet) {
-            sum += j;
-            System.out.println(sum);
-        }
-        ;
-        System.out.println(linkedHashSet);
-
-        return sum / linkedHashSet.size();
+        return (double) sum / arr.length;
     };
 
+    /**
+     * A method to determine the median number in a given array of numbers
+     * 
+     * @param arr an array of numbers(double)
+     * @return the median number of the array
+     */
     public static double median(double[] arr) {
         if (arr.length == 1) {
             return arr[0];
         }
-        ;
-        double[] sortableArray = arr;
-        Arrays.sort(sortableArray);
+
         if (arr.length % 2 != 0) {
-            double middle = sortableArray.length / 2 - 0.5;
-            return sortableArray[(int) middle];
+            double middle = (arr.length / 2);
+            return arr[(int) middle];
         } else {
-            double middleLeft = sortableArray.length / 2 - 0.5;
-            double middleRight = sortableArray.length / 2 + 0.5;
-            return (sortableArray[(int) middleLeft] + sortableArray[(int) middleRight]) / 2;
+            double middleLeft = arr.length / 2 - 0.5;
+            double middleRight = arr.length / 2 + 0.5;
+            return (arr[(int) middleLeft] + arr[(int) middleRight]) / 2;
         }
+
+        // In case the array needs sorting, guidelines unclear
+//      double[] sortableArray = arr;
+//      Arrays.sort(sortableArray);
+//        if (arr.length % 2 != 0) {
+//            double middle = (sortableArray.length / 2);
+//            return sortableArray[(int) middle];
+//        } else {
+//            double middleLeft = sortableArray.length / 2 - 0.5;
+//            double middleRight = sortableArray.length / 2 + 0.5;
+//            return (sortableArray[(int) middleLeft] + sortableArray[(int) middleRight]) / 2;
+//        }
     };
 
+    /**
+     * A method to determine the factorial of a number below 20
+     *
+     * @param num a number below 20
+     * @throws illegalArugmentException if the supplied argument is greater than 20
+     * @return the factorial of the given number
+     * 
+     */
     public static long factorial(long num) {
         if (num == 0) {
             return 1;
@@ -113,6 +141,12 @@ public class Numbers {
         return fact;
     };
 
+    /**
+     * A method to determine the factorial of a larger number
+     * 
+     * @param num a number
+     * @return the factorial of the supplied number
+     */
     public static BigInteger factorial(BigInteger num) {
         if (num == BigInteger.ZERO || num == BigInteger.ONE) {
             return BigInteger.ONE;
